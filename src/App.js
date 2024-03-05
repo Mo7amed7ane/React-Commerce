0fac767e996039089399f78fa97864a95067b428
Mohamed Hane <hany84641@gmail.com>
2024-03-05 12:42:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Layout from "./Components/Layout";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Home from "./Components/Home/Home";
+import Product from "./Components/Product/Product";
+import Categories from "./Components/Categories/Categories";
+import Brands from "./Components/Brands/Brands";
+import NotFound from "./Components/NotFound";
+import { RouterProvider, createBrowserRouter, createHashRouter } from "react-router-dom";
+
+import { useContext, useEffect } from "react";
+import { userContext } from "./UserContext";
+import ProtectedRouute from "./ProtectedRoute";
+import Cart from './Components/Cart/Cart';
+import ProductDetails from './productDetails';
+import Orders from "./Orders";
+
 
 function App() {
+  let {setIsUser,setLogin} = useContext(userContext)
+
+  useEffect(()=> {
+  if (localStorage.getItem('userToken'))
+  setIsUser(localStorage.getItem('userToken'))
+  setLogin(localStorage.getItem('userName'))
+
+  },[]);
+
+
+
+
+
+  const routes = createHashRouter([
+    {
+      path: "",
+      element: <Layout></Layout>,
+      children: [
+        { index: true, element: <Login></Login> },
+        { path: "register", element: <Register></Register> },
+        { path: "home", element: <ProtectedRouute> <Home /> </ProtectedRouute> },
+        { path: "cart", element: <ProtectedRouute> <Cart /> </ProtectedRouute> },
+        { path: "allorders", element: <ProtectedRouute> <Orders /> </ProtectedRouute> },
+        { path: "productDetails/:id", element: <ProtectedRouute> <ProductDetails></ProductDetails></ProtectedRouute> },
+        { path: "product", element: <ProtectedRouute> <Product></Product> </ProtectedRouute>},
+        { path: "categories", element:<ProtectedRouute> <Categories></Categories></ProtectedRouute> },
+        { path: "brands", element: <ProtectedRouute><Brands></Brands></ProtectedRouute> },
+        { path: "*", element: <NotFound></NotFound> },
+        { path: "login", element: <Login></Login> },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={routes}></RouterProvider>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import  { Toaster } from 'react-hot-toast';
+import { UserContextProvider } from './UserContext';
+import {QueryClient, QueryClientProvider} from 'react-query'
+
+
+const queryClient = new QueryClient({defaultOptions:{queries:{ cacheTime: 600000 ,
+        refetchOnWindowFocus:false,
+        }}})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<UserContextProvider>
+     
+   <QueryClientProvider client={queryClient}>
+   <Toaster   position="top-left"
+  reverseOrder={false} />
+        <App />
+       
+        </QueryClientProvider>
+</UserContextProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
